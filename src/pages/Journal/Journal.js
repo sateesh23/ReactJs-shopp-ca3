461a5b596cd481e4ad7687ea3ad0a6b5a7f2b6bb
Sateesh Gavara <114515067+sateesh23@users.noreply.github.com>
2024-11-13 10:31:14 +0530

    commit

---
+++ src/pages/Journal/Journal.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
+
+const Journal = () => {
+  const location = useLocation();
+  const [prevLocation, setPrevLocation] = useState("");
+  useEffect(() => {
+    setPrevLocation(location.state.data);
+  }, [location]);
+  return (
+    <div className="max-w-container mx-auto px-4">
+      <Breadcrumbs title="Journals" prevLocation={prevLocation} />
+      <div className="pb-10">
+        <h1 className="max-w-[600px] text-base text-lightText mb-2">
+          <span className="text-primeColor font-semibold text-lg">TWENTYZ</span>{" "}
+          This is a sample journal entry of twentyz ecommerce website
+          you can order any product from this website. so grab offers now!
+        </h1>
+        <Link to="/shop">
+          <button className="w-52 h-10 bg-primeColor text-white hover:bg-black duration-300">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Journal;
